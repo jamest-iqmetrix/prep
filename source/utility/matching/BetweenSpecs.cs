ba22ae474a00c61d93e73b15de8470c3037aa456
James Trenchard <jamest@iqmetrix.com>
2017-03-21 17:02:39 -0500

    Pushing new changes

--- source/utility/matching/BetweenSpecs.cs
+++ source/utility/matching/BetweenSpecs.cs
@@ -2,31 +2,23 @@
 
 namespace code.utility.matching
 {
-  [Subject(typeof(Between))]
   class BetweenSpecs
   {
-    public class SomeType
-    {
-    }
-
     public class when_creating_its_strategy
     {
+      static protected Criteria<int> criteria;
+
+      private Because b = () =>
+      {
+        criteria = Between.values(2, 8);
+      };
+
       It returns_a_criteria_the_matches_values_between_the_provided_values = () =>
       {
-        var criteria = Between.values(2, 8);
-
         criteria(4).ShouldBeTrue();
         criteria(9).ShouldBeFalse();
       };
     }
-
-    public class when_creating_a_ReturnType
-    {
-
-      Establish c = () =>
-      {
-
-      };
-    }
+    
   }
 }
