5bf8e232ec2967f199b2681d3123423f7623f381
James Trenchard <jamest@iqmetrix.com>
2017-03-21 16:53:50 -0500

    Pushing new changes

--- source/utility/matching/Between.cs
+++ source/utility/matching/Between.cs
@@ -12,11 +12,11 @@ namespace code.utility.matching
 
   public static class BetweenMatchingExtensions
   {
-    public static ReturnType between<Item,Property, ReturnType>(this IProvideAccessToMatchBuilders<Item,Property, ReturnType> extension_point, Property start, Property end) where Property : IComparable<Property>
+    public static ReturnType between<Item, Property, ReturnType>(
+      this IProvideAccessToMatchBuilders<Item, Property, ReturnType> extension_point, Property start, Property end)
+      where Property : IComparable<Property>
     {
       return extension_point.create(Between.values(start, end));
     }
-
   }
- 
 }
---
+++ source/utility/matching/BetweenSpecs.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+
+namespace code.utility.matching
+{
+  [Subject(typeof(Between))]
+  class BetweenSpecs
+  {
+    public class SomeType
+    {
+    }
+
+    public class when_creating_its_strategy
+    {
+      It returns_a_criteria_the_matches_values_between_the_provided_values = () =>
+      {
+        var criteria = Between.values(2, 8);
+
+        criteria(4).ShouldBeTrue();
+        criteria(9).ShouldBeFalse();
+      };
+    }
+
+    public class when_creating_a_ReturnType
+    {
+
+      Establish c = () =>
+      {
+
+      };
+    }
+  }
+}
