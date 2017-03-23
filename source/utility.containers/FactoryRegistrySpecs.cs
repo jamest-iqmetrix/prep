009b9d516ab5beeb724e14ce499a3f0bcc05f8e8
James Trenchard <jamest@iqmetrix.com>
2017-03-23 17:45:55 -0500

    Pushing new changes

---
+++ source/utility.containers/FactoryRegistrySpecs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using code.utility.containers;
+using developwithpassion.specifications.assertions.interactions;
+using Machine.Specifications;
+using spec = developwithpassion.specifications.use_engine<Machine.Fakes.Adapters.Rhinomocks.RhinoFakeEngine>;
+
+namespace Namespace
+{
+  [Subject(typeof(FactoryRegistry))]
+  public class FactoryRegistrySpecs
+  {
+    public class concern : spec.observe<IFindFactoriesForAType, FactoryRegistry>
+    {
+    }
+
+    public class when_getting_factory_for_a_type : concern
+    {
+      class SomeType{}
+
+      Establish context = () =>
+      {
+        the_appropriate_factory = fake.an<ICreateOneDependency>();
+        factory_dictionary = depends.on<IDictionary<Type, ICreateOneDependency>>();
+        factory_dictionary.Add(typeof(SomeType), the_appropriate_factory);
+      };
+
+      Because of = () =>
+      {
+        result = sut.get_factory_that_can_create(typeof(SomeType));
+      };
+
+      It should_return_the_appropriate_factory = () =>
+      {
+        result.ShouldEqual(the_appropriate_factory);
+      };
+
+      static IDictionary<Type, ICreateOneDependency> factory_dictionary;
+      static ICreateOneDependency the_appropriate_factory;
+      static ICreateOneDependency result;
+    }
+  }
+}
